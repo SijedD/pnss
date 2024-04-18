bdeaf2d7333c988707a90de9312cc63f92095b4c
SIJED <Danilcarol56@gmail.com>
2024-04-18 20:46:03 +0700

    pr6 auth

--- pop-it-mvc/app/Controller/Api.php
+++ pop-it-mvc/app/Controller/Api.php
@@ -2,9 +2,12 @@
 
 namespace Controller;
 
+use Illuminate\Support\Str;
 use Model\Post;
+use Src\Auth\Auth;
 use Src\Request;
 use Src\View;
+
 
 class Api
 {
@@ -19,4 +22,27 @@ class Api
     {
         (new View())->toJSON($request->all());
     }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->all();
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = Str::random(60);
+
+            $user->api_token = hash('sha256', $token); //хеш-сумма
+            $user->save();
+
+            (new View())->toJSON(['token' => $token], 200);
+        }
+
+        (new View())->toJSON(['message' => 'Unauthorized'], 401);
+    }
 }
+
+//{
+ //   "login" : "123",
+ //   "password": "123"
+
+//}
