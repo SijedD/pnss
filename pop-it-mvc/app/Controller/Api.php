7b36ddb3bbaec82ccfb08c16ee09830c4603e763
gr421_vidol <Danilcarol56@gmail.com>
2024-04-19 13:17:43 +0700

    .

--- pop-it-mvc/app/Controller/Api.php
+++ pop-it-mvc/app/Controller/Api.php
@@ -4,8 +4,10 @@ namespace Controller;
 
 use Illuminate\Support\Str;
 use Model\Post;
+use Model\Subscriber;
 use Src\Auth\Auth;
 use Src\Request;
+use Src\Validator\Validator;
 use Src\View;
 
 
@@ -31,7 +33,7 @@ class Api
             $user = Auth::user();
             $token = Str::random(60);
 
-            $user->api_token = hash('sha256', $token); //хеш-сумма
+            $user->api_token = hash('md5', $token); //хеш-сумма
             $user->save();
 
             (new View())->toJSON(['token' => $token], 200);
@@ -39,10 +41,34 @@ class Api
 
         (new View())->toJSON(['message' => 'Unauthorized'], 401);
     }
+
+    public function addUser(Request $request)
+    {
+        if ($request->method === 'POST') {
+            $validator = new Validator($request->all(), [
+                'name' => ['required'],
+                'surname' => ['required'],
+                'patronymic' => ['required'],
+                'date_birth' => ['required'],
+            ], [
+                'required' => 'Поле :field пусто',
+                'unique' => 'Поле :field должно быть уникально'
+            ]);
+            if ($validator->fails()) {
+                (new View())->toJSON(['message' => $validator->errors()], 422);
+            }
+            $subscriber = Subscriber::create($request->all());
+            if ($subscriber) {
+                (new View())->toJSON(['message' => "user add"], 200);
+            }
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $user = Auth::user();
+        $user->api_token = '';
+        $user->save();
+        (new View())->toJSON(['message' => 'successful'], 200);
+    }
 }
-
-//{
- //   "login" : "123",
- //   "password": "123"
-
-//}
